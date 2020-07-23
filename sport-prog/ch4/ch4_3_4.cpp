a6ac2eedd7bc6cd21384f54f054c46b229c675a3
Amir Farukshin <farukshin.amir@gmail.com>
2020-07-23 22:22:54 +0300

    docs(): изменил архитектуру решения, остался Runtime error на 11 тест-кейсе (частичное решение)

--- sport-prog/ch4/ch4_3_4.cpp
+++ sport-prog/ch4/ch4_3_4.cpp
@@ -8,65 +8,96 @@ typedef long double ld;
 const int MAX = 1005;
 int n, m;
 char ans[MAX][MAX] = {' '};
+bool used[MAX][MAX] = {false};
 
-void getSteps(pair<int, int> start, vector<pair<int, int>> &steps)
+struct slide
 {
-    int x, y;
+    pair<int, int> start;
+    pair<int, int> parrent;
+};
+queue<slide> que;
+
+bool nearWall(pair<int, int> s)
+{
+    if (!s.first % 2 || !s.second % 2)
+        return false;
+
+    bool nearWall = ans[s.first][s.second - 1] == '|' || ans[s.first][s.second + 1] == '|' || ans[s.first - 1][s.second] == '-' || ans[s.first + 1][s.second] == '-';
+
+    return nearWall;
+}
+
+void pushQue(pair<int, int> start, pair<int, int> par)
+{
+    slide s;
+    s.start = start;
+    s.parrent = par;
+    que.push(s);
+}
+
+bool check(const pair<int, int> &s)
+{
+    bool check = (s.first >= 0) && (s.first < (2 * n + 1)) && (s.second >= 0) && (s.second < (2 * m + 1));
+    return check;
+}
+
+void runSlider(slide sl)
+{
+
     pair<int, int> run[4] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     for (auto r : run)
     {
-        x = start.first, y = start.second;
+        pair<int, int> next = sl.start, pre = sl.start, behind = sl.start;
 
-        while (true)
+        next.first += r.first;
+        next.second += r.second;
+
+        behind.first += -1 * r.first;
+        behind.second += -1 * r.second;
+
+        // if (!check(behind) || !check(next))
+        //     continue;
+
+        if (next.first == sl.parrent.first && next.second == sl.parrent.second)
+            continue;
+
+        if (!(ans[behind.first][behind.second] == '|' || ans[behind.first][behind.second] == '-'))
+            continue;
+
+        while (ans[next.first][next.second] == ' ' || ans[next.first][next.second] == 'S')
         {
-            char ch = ans[x + r.first][y + r.second];
-            if (ch == ' ' || ch == 'S' || ch == 'D')
-                x += r.first, y += r.second;
-            else
-                break;
+
+            if (nearWall(next) && !used[next.first][next.second])
+                pushQue(next, pre);
+
+            used[next.first][next.second] = true;
+
+            pre = next;
+            next.first += r.first;
+            next.second += r.second;
+
+            // if (!check(next))
+            //     break;
         }
-        steps.push_back({x, y});
     }
 }
 
-bool bfs(pair<int, int> start, pair<int, int> finish)
+void bfs(slide sl)
 {
-    queue<pair<int, int>> que;
-    que.push({start.first, start.second});
-
-    bool used[MAX][MAX] = {false};
-    used[start.first][start.second] = true;
-
+    que.push(sl);
     while (!que.empty())
     {
         auto cur = que.front();
         que.pop();
-
-        if (cur.first == finish.first && cur.second == finish.second)
-            return true;
-
-        vector<pair<int, int>> steps;
-        getSteps(cur, steps);
-
-        for (auto r : steps)
-            if (!used[r.first][r.second])
-            {
-                used[r.first][r.second] = true;
-                que.push({r.first, r.second});
-            }
+        runSlider(cur);
     }
-
-    return false;
 }
 
 void solve()
 {
     int n, m;
     cin >> n >> m;
-
-    //char ans[2 * n + 1][2 * m + 1] = {' '};
-    //bool used[n + 2][m + 2] = {false};
 
     pair<int, int> stock;
     vector<pair<int, int>> d;
@@ -82,21 +113,24 @@ void solve()
             if (s[j] == 'S')
                 stock = {i, j};
             if (s[j] == 'D')
+            {
                 d.push_back({i, j});
+                ans[i][j] = ' ';
+            }
         }
     }
 
+    bfs({stock, {-1, -1}});
+
     for (auto ut : d)
-        if (!bfs(ut, stock))
-            ans[ut.first][ut.second] = ' ';
+        if (used[ut.first][ut.second])
+            ans[ut.first][ut.second] = 'D';
 
     for (int i = 0; i < 2 * n + 1; i++)
-    {
         for (int j = 0; j < 2 * m + 1; j++)
-            cout << ans[i][j];
-        cout << endl;
-    }
+            cout << ans[i][j] << (j == 2 * m ? "\n" : "");
 }
+
 int main()
 {
     ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
