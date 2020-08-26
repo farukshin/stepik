d900e924d4c9a255b3cd821b8d13955ea77ed63e
Amir Farukshin <farukshin.amir@gmail.com>
2020-08-26 19:38:51 +0300

    docs(): зафиксировал текущее решение задач

---
+++ task_cpp/ch2/ch2_12_7.cpp
@@ -0,0 +1,63 @@
+//#tech_debt
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+int movFormat(int inp, int out, string num) {
+
+    int k = 1;
+    int res = 0;
+    for (int i = num.length() - 1; i >= 0; i--)
+    {
+        res += (num[i] - '0') * k;
+        k *= inp;
+    }
+    if (out == 10)
+        return res;
+
+    k = 1;
+    while (k <= res)
+        k *= out;
+
+    int ans = 0;
+    int k10 = 1;
+    while (res) {
+        ans = ans * 10 + (res / k);
+        res -= (res / k) * k;
+        k /= out;
+    }
+    while (k == 1) {
+        ans *= 10;
+        k /= out;
+    }
+
+    return ans;
+}
+
+void solve()
+{
+    string a, b;
+    cin >> a >> b;
+    int a10 = movFormat(2, 10, a);
+    int b10 = movFormat(3, 10, b);
+    int c10 = movFormat(10, 4, to_string(a10 + b10));
+
+    printf("%d + %d = %d\n%d\n", a10, b10, a10 + b10, c10);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
